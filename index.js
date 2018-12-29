b895cd54a04b1b9d46120e98c38487634f224334
Peter Pang <mv@live.cn>
2018-12-29 17:19:42 +0800

    Add yaml.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+app.get('/', (req, res)=>{
+    res.send({isSuccessful:true});
+});
+
 app.get('/api/signin', (req, res) => {
     if(req.header('token')=='fewbox'){
         res.send({isSuccessful:true, isValid:true});
